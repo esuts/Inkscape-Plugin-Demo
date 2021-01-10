16665805acd4ab2ce8c5d4cdd205bee1193416b4
eric <eric@Eric-PC>
2021-01-10 12:14:01 -0500

    Ignore the project files

--- cube-cutout.groovy
+++ cube-cutout.groovy
@@ -1,1 +1,34 @@
-//Your code here
+import eu.mihosoft.vrl.v3d.svg.*;
+
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
+
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Extrude;
+import eu.mihosoft.vrl.v3d.Polygon
+
+File f = ScriptingEngine
+	.fileFromGit(
+		"https://github.com/esuts/Inkscape-Plugin-Demo.git",//git repo URL
+		"master",//branch
+		"Inkscape-Plugin-Demo.SVG"// File from within the Git repo
+	)
+println "Extruding SVG "+f.getAbsolutePath()
+SVGLoad s = new SVGLoad(f.toURI())
+println "Layers= "+s.getLayers()
+// A map of layers to polygons
+HashMap<String,List<Polygon>> polygonsByLayer = s.toPolygons()
+// extrude all layers to a map to 10mm thick
+HashMap<String,ArrayList<CSG>> csgByLayers = s.extrudeLayers(10)
+// extrude just one layer to 10mm
+def holeParts = s.extrudeLayerToCSG(10,"Slice 1")
+def holeParts = s.extrudeLayerToCSG(10,"Slice 2")
+// seperate holes and outsides using layers to differentiate
+
+/*
+def outsideParts = s.extrudeLayerToCSG(10,"Slice 2")
+					.difference(holeParts)
+// layers can be extruded at different depths					
+def boarderParts = s.extrudeLayerToCSG(5,"3-boarder")
+*/
+
+return [CSG.unionAll([boarderParts,outsideParts]),s.extrudeLayerToCSG(2,"4-star")]
